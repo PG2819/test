b6b621d26294e8454cda33a3193ac7814b3e2dcc
PG2819 <251158365@qq.com>
2018-11-26 19:36:38 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stafx.h>
+#include <iostream>
+using namespace std;
+templete<class T>
+        class chain;
+templete<class T>
+        class chainnode;
+
+templete<class T>
+int main() {
+    cout << "Hello World!"<< endl;
+    return 0;
+}
